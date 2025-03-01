09187f89dd5e7c14f5881db813e4cb67436d7a1f
d1eg-00 <dieego.fernandes@hotmail.com>
2025-03-01 02:11:28 -0300

    Adicionando README.md e configurações iniciais

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+  string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection")!;
+
+  builder.Services.AddDbContext<AppDbContext>(Options => 
+                      Options.UseMySql(mySqlConnection, 
+                      ServerVersion.AutoDetect(mySqlConnection)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+  app.UseSwagger();
+  app.UseSwaggerUI();
+    //app.MapOpenApi();
+    //app.UseSwaggerUI(options => 
+    //options.SwaggerEndpoint("/openapi/v1.json", "weather api"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
