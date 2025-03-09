22bf2063a55cf2f5f26fc7bee7dad5a9f30ef979
d1eg-00 <dieego.fernandes@hotmail.com>
2025-03-08 22:42:05 -0300

    incluindo serviços dataanotations

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -14,17 +14,18 @@ namespace APICatalogo.Models
     {
         [Key]
         public int ProdutoId { get; set; }
-        [Required]
-        [StringLength(80)]
+        [Required(ErrorMessage = "Nome do Produto Obrigatório!")]
+        [StringLength(10, ErrorMessage = "O Nome Deve Conter no Mínimo 5 Caracteres e no Máximo 10!", MinimumLength = 5)]
         public string ?Nome { get; set; }
-        [Required]
-        [StringLength(300)]
+        [Required(ErrorMessage = "Descrição do Produto Obrigatório!")]
+        [StringLength(80, ErrorMessage = "A Descrição deve ter no máximo {1} Caracteres!")]
         public string ?Descricao { get; set; }
         [Required]
+        [Range(1,10000, ErrorMessage="O Preço deve estar entre {1} e {2}")]
         [Column(TypeName ="decimal(10,2)")]
         public decimal Preco { get; set; }
         [Required]
-        [StringLength(300)]
+        [StringLength(300, MinimumLength = 10)]
         public string ?ImageUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
